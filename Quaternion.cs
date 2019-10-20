423ad5bf0fd82abfe73e89fa5d47ba30adf7991b
Naomi <614231+healingbrew@users.noreply.github.com>
2019-10-20 05:13:08 +0100

    [math] Add Math types

---
+++ Quaternion.cs
@@ -0,0 +1,12 @@
+﻿namespace DragonLib
+{
+    public struct Quaternion<T>
+    {
+        public T X { get; set; }
+        public T Y { get; set; }
+        public T Z { get; set; }
+        public T W { get; set; }
+
+        public (T x, T y, T z, T w) AsTuple() => (X, Y, Z, W);
+    }
+}
---
+++ Vector.cs
@@ -0,0 +1,11 @@
+﻿namespace DragonLib
+{
+    public struct Vector<T>
+    {
+        public T X { get; set; }
+        public T Y { get; set; }
+        public T Z { get; set; }
+
+        public (T x, T y, T z) AsTuple() => (X, Y, Z);
+    }
+}
