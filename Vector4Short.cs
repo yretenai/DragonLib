ea430543a47135da0116f093384a3c04cdb952bd
Naomi <614231+healingbrew@users.noreply.github.com>
2019-10-24 07:17:20 +0100

    Remove struct formats added by GLTF

---
+++ Vector4Short.cs
@@ -0,0 +1,10 @@
+namespace DragonLib
+{
+    public struct Vector4Short
+    {
+        public short X { get; set; }
+        public short Y { get; set; }
+        public short Z { get; set; }
+        public short W { get; set; }
+    }
+}
