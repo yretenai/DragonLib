2fe9f72a7f48d7207b82162770228d0718b427f4
Naomi <614231+healingbrew@users.noreply.github.com>
2020-04-28 01:29:27 +0100

    [System.Text.Json] Add JsonConverter factory for generic objects

---
+++ JSON/GenericDictionaryConverter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using JetBrains.Annotations;
+
+namespace DragonLib.JSON
+{
+    [PublicAPI]
+    public class GenericDictionaryConverter<T> : JsonConverter<Dictionary<string, T>>
+    {
+        public override Dictionary<string, T> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType != JsonTokenType.StartObject) throw new JsonException();
+
+            var dict = new Dictionary<string, T>();
+            while (reader.Read())
+            {
+                if (reader.TokenType == JsonTokenType.EndObject) return dict;
+                if (reader.TokenType != JsonTokenType.PropertyName) throw new JsonException();
+
+                var key = reader.GetString();
+                reader.Read();
+                if (reader.TokenType != JsonTokenType.StartArray) throw new JsonException();
+
+                reader.Read();
+                var typeinfo = reader.GetString() ?? throw new JsonException();
+                reader.Read();
+                var type = Type.GetType(typeinfo) ?? throw new TypeUnloadedException();
+                var value = JsonSerializer.Deserialize(ref reader, type, options);
+                dict[key!] = (T) value!;
+                reader.Read();
+                if (reader.TokenType != JsonTokenType.EndArray) throw new JsonException();
+            }
+
+            return dict;
+        }
+
+        public override void Write(Utf8JsonWriter writer, Dictionary<string, T> dict, JsonSerializerOptions options)
+        {
+            writer.WriteStartObject();
+            foreach (var (key, value) in dict)
+            {
+                writer.WritePropertyName(key);
+                writer.WriteStartArray();
+                writer.WriteStringValue(value?.GetType().AssemblyQualifiedName);
+                JsonSerializer.Serialize(writer, value, options);
+                writer.WriteEndArray();
+            }
+
+            writer.WriteEndObject();
+        }
+    }
+}
---
+++ JSON/GenericDictionaryConverterFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using JetBrains.Annotations;
+
+namespace DragonLib.JSON
+{
+    [PublicAPI]
+    public class GenericDictionaryConverterFactory : JsonConverterFactory
+    {
+        public override bool CanConvert(Type typeToConvert)
+        {
+            if (!typeToConvert.IsGenericType) return false;
+
+            return typeToConvert.GetGenericTypeDefinition() == typeof(Dictionary<,>) && typeToConvert.GetGenericArguments()[0].IsEquivalentTo(typeof(string));
+        }
+
+        public override JsonConverter CreateConverter(Type typeToConvert, JsonSerializerOptions options) => (JsonConverter) (Activator.CreateInstance(typeof(GenericDictionaryConverter<>).MakeGenericType(typeToConvert.GetGenericArguments()[1])) ?? throw new JsonException());
+    }
+}
