d22665398b545f14ff3e262701c511fd340ab400
Naomi Ahmed <yretenai@gmail.com>
2021-12-13 16:17:35 +0000

    Use MemoryMarshal.AsBytes

--- Hashing/FNV.cs
+++ Hashing/FNV.cs
@@ -9,7 +9,7 @@ namespace DragonLib.Hashing {
         public const uint FNV1Prime32Bit = 0x01000193U;
 
         public static ulong FNV1UI64<T>(Span<T> data, ulong basis = FNV1Basis64Bit, ulong prime = FNV1Prime64Bit) where T : struct {
-            var byteData = MemoryMarshal.Cast<T, byte>(data);
+            var byteData = MemoryMarshal.AsBytes(data);
             var hash = basis;
             foreach (var b in byteData) {
                 hash *= prime;
@@ -20,7 +20,7 @@ namespace DragonLib.Hashing {
         }
 
         public static uint FNV1UI32<T>(Span<T> data, uint basis = FNV1Basis32Bit, uint prime = FNV1Prime32Bit) where T : struct {
-            var byteData = MemoryMarshal.Cast<T, byte>(data);
+            var byteData = MemoryMarshal.AsBytes(data);
             var hash = basis;
             foreach (var b in byteData) {
                 hash *= prime;
@@ -32,7 +32,7 @@ namespace DragonLib.Hashing {
 
         // ReSharper disable once InconsistentNaming
         public static ulong FNV1aUI64<T>(Span<T> data, ulong basis = FNV1Basis64Bit, ulong prime = FNV1Prime64Bit) where T : struct {
-            var byteData = MemoryMarshal.Cast<T, byte>(data);
+            var byteData = MemoryMarshal.AsBytes(data);
             var hash = basis;
             foreach (var b in byteData) {
                 hash ^= b;
@@ -44,7 +44,7 @@ namespace DragonLib.Hashing {
 
         // ReSharper disable once InconsistentNaming
         public static uint FNV1aUI32<T>(Span<T> data, uint basis = FNV1Basis32Bit, uint prime = FNV1Prime32Bit) where T : struct {
-            var byteData = MemoryMarshal.Cast<T, byte>(data);
+            var byteData = MemoryMarshal.AsBytes(data);
             var hash = basis;
             foreach (var b in byteData) {
                 hash ^= b;
