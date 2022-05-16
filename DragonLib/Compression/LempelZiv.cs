7c98077546e4b6f741735c203be78afe76432408
Naomi Ahmed <yretenai@gmail.com>
2022-05-16 21:23:31 +0100

    Attribute LZ4 source, add LZF.

--- DragonLib/Compression/LempelZiv.cs
+++ DragonLib/Compression/LempelZiv.cs
@@ -1,6 +1,8 @@
 ﻿namespace DragonLib.Compression;
 
 public static class LempelZiv {
+    // https://github.com/Thealexbarney/LibHac/blob/e0b482f44b5f225cfe29af0b80186bcb8895806c/src/LibHac/Util/Lz4.cs
+    // modifications: exit on overflow on either cmp or dec, return pos cursors.
     public static (int CmpPos, int DecPos) DecompressLZ4(ReadOnlySpan<byte> cmp, Span<byte> dec) {
         var cmpPos = 0;
         var decPos = 0;
@@ -75,6 +77,49 @@ public static class LempelZiv {
             }
         } while (cmpPos < cmp.Length &&
                  decPos < dec.Length);
+
+        return (cmpPos, decPos);
+    }
+
+    // some old code, idk where it's from.
+    public static (int CmpPos, int DecPos) DecompressLZF(ReadOnlySpan<byte> cmp, Span<byte> dec) {
+        var cmpPos = 0;
+        var decPos = 0;
+
+        while (cmpPos < cmp.Length && decPos < dec.Length) {
+            var token = cmp[cmpPos++];
+            if (token <= 0x1F) {
+                for (int i = token; i >= 0; --i) {
+                    dec[decPos] = cmp[cmpPos];
+                    ++decPos;
+                    ++cmpPos;
+
+                    if (cmpPos >= cmp.Length || decPos >= dec.Length) {
+                        break;
+                    }
+                }
+            } else {
+                var encLen = token >> 5;
+                if (encLen == 7) {
+                    encLen += cmp[cmpPos++];
+                }
+
+                var dictDist = ((token & 0x1f) << 8) | cmp[cmpPos];
+                ++cmpPos;
+                encLen += 2;
+
+                var encPos = decPos - 1 - dictDist;
+
+                if (encPos + encLen > dec.Length || decPos + encLen > dec.Length) {
+                    encLen = dec.Length - encPos;
+                }
+
+                for (var i = 0; i < encLen; ++i) {
+                    dec[decPos] = dec[encPos + i];
+                    ++decPos;
+                }
+            }
+        }
 
         return (cmpPos, decPos);
     }
