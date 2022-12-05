cbb166f531f1cbf1294adabc14251207c4df8475
Naomi Ahmed <yretenai@gmail.com>
2022-12-05 20:41:03 +0000

    Some safety stuff, allow to check equivalence between different array types.

--- DragonLib/Unsafe/Array.cs
+++ DragonLib/Unsafe/Array.cs
@@ -3,34 +3,93 @@ using System.Runtime.InteropServices;
 
 namespace DragonLib.Unsafe;
 
-public sealed class Array<T> : IDisposable, IEnumerable<T>, IEquatable<Array<T>> where T : unmanaged {
-    public unsafe delegate void FreeDelegate(void* ptr);
+public abstract class ArrayBase : IEquatable<ArrayBase> {
+    public readonly int Length;
+    protected nint Ptr;
 
-    private readonly FreeDelegate FreeInner;
+    protected ArrayBase(int length) => Length = length;
 
-    public int Length;
-    public nint Pointer;
-
-    private Array(nint pointer, int length, FreeDelegate free) {
-        if (pointer == 0) {
-            throw new ArgumentException("Pointer cannot be null", nameof(pointer));
+    public bool Equals(ArrayBase? other) {
+        if (ReferenceEquals(null, other)) {
+            return false;
         }
 
-        Pointer = pointer;
-        Length = length;
-        FreeInner = free;
+        if (ReferenceEquals(this, other)) {
+            return true;
+        }
+
+        return Ptr == other.Ptr && Length == other.Length;
     }
 
-    public unsafe Span<T> Span => new((T*)Pointer, Length);
+    public override bool Equals(object? obj) => obj is ArrayBase other && Equals(other);
+    public override int GetHashCode() => Ptr.GetHashCode();
+    public static bool operator ==(ArrayBase? left, ArrayBase? right) => Equals(left, right);
+    public static bool operator !=(ArrayBase? left, ArrayBase? right) => !Equals(left, right);
+
+    public virtual unsafe Array<T> Cast<T>() where T : unmanaged => new(Ptr, Length, Array<T>.NullFree);
+}
+
+public unsafe delegate void FreeArrayDelegate(void* ptr);
+
+public sealed class Array<T> : ArrayBase, IDisposable, IEnumerable<T>, IEquatable<Array<T>> where T : unmanaged {
+    private readonly FreeArrayDelegate FreeArrayInner;
+
+    private unsafe Array() : base(0) {
+        Ptr = 0;
+        FreeArrayInner = NullFree;
+    }
+
+    public Array(nint ptr, int length, FreeArrayDelegate freeArray) : base(length) {
+        if (ptr == 0) {
+            throw new ArgumentException("Pointer cannot be null", nameof(ptr));
+        }
+
+        if (length < 0) {
+            throw new ArgumentException("Length cannot be negative", nameof(length));
+        }
+
+        Ptr = ptr;
+        FreeArrayInner = freeArray;
+    }
+
+    public static Array<T> Empty { get; } = new();
+
+    public bool IsEmpty => Length == 0 || Ptr == 0;
+
+    public unsafe Span<T> Span {
+        get {
+            ThrowIfDisposed();
+
+            return IsEmpty ? Span<T>.Empty : new Span<T>((T*) Ptr, Length);
+        }
+    }
+
+    public unsafe T* Pointer {
+        get {
+            ThrowIfDisposed();
+
+            if (IsEmpty) {
+                return null;
+            }
+
+            return (T*) Ptr;
+        }
+    }
+
+    public Memory<T> Memory => ToManaged().AsMemory();
 
     public unsafe ref T this[int index] {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         get {
+            ThrowIfDisposed();
+
             if (index >= Length || index < 0) {
-                throw new InvalidOperationException();
+#pragma warning disable CA1065
+                throw new IndexOutOfRangeException();
+#pragma warning restore CA1065
             }
 
-            return ref ((T*)Pointer)[index];
+            return ref ((T*) Ptr)[index];
         }
     }
 
@@ -40,6 +99,8 @@ public sealed class Array<T> : IDisposable, IEnumerable<T>, IEquatable<Array<T>>
     }
 
     public IEnumerator<T> GetEnumerator() {
+        ThrowIfDisposed();
+
         for (var i = 0; i < Length; ++i) {
             yield return this[i];
         }
@@ -47,14 +108,41 @@ public sealed class Array<T> : IDisposable, IEnumerable<T>, IEquatable<Array<T>>
 
     IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
-    public bool Equals(Array<T>? other) => other?.Pointer == Pointer && other.Length == Length;
+    public bool Equals(Array<T>? other) => base.Equals(other) && FreeArrayInner.Equals(other.FreeArrayInner);
+
+    public static unsafe void NullFree(void* ptr) { }
+
+    private void ThrowIfDisposed() {
+        if (Ptr == 0 && Length != 0) {
+            throw new ObjectDisposedException(nameof(Array<T>));
+        }
+    }
 
     public unsafe T[] ToManaged() {
-        var array = new T[Length];
-        fixed (T* ptr = array) {
-            NativeMemory.Copy((void*)Pointer, ptr, (uint)(Length * sizeof(T)));
+        ThrowIfDisposed();
+
+        if (IsEmpty) {
+            return Array.Empty<T>();
         }
 
+        var array = new T[Length];
+        fixed (T* ptr = array) {
+            NativeMemory.Copy((void*) Ptr, ptr, (uint) (Length * sizeof(T)));
+        }
+
+        return array;
+    }
+
+    public unsafe Array<T> ToAligned(int alignment = 16) {
+        ThrowIfDisposed();
+
+        if (IsEmpty || Ptr % alignment == 0) {
+            return this;
+        }
+
+        var array = AllocAligned(Length, alignment);
+        NativeMemory.Copy((void*) Ptr, (void*) array.Ptr, (uint) (Length * sizeof(T)));
+        Dispose();
         return array;
     }
 
@@ -62,30 +150,20 @@ public sealed class Array<T> : IDisposable, IEnumerable<T>, IEquatable<Array<T>>
         Free();
     }
 
-    public static unsafe Array<T> Alloc(int length) => new((nint)NativeMemory.Alloc((nuint)(length * System.Runtime.CompilerServices.Unsafe.SizeOf<T>())), length, NativeMemory.Free);
-    public static unsafe Array<T> AllocZeroed(int length) => new((nint)NativeMemory.AllocZeroed((nuint)(length * System.Runtime.CompilerServices.Unsafe.SizeOf<T>())), length, NativeMemory.Free);
-    public static unsafe Array<T> AllocAligned(int length, int alignment = 16) => new((nint)NativeMemory.AlignedAlloc((nuint)(length * System.Runtime.CompilerServices.Unsafe.SizeOf<T>()), (nuint)alignment), length, NativeMemory.AlignedFree);
+    public static unsafe Array<T> Alloc(int length) => new((nint) NativeMemory.Alloc((nuint) (length * System.Runtime.CompilerServices.Unsafe.SizeOf<T>())), length, NativeMemory.Free);
+    public static unsafe Array<T> AllocZeroed(int length) => new((nint) NativeMemory.AllocZeroed((nuint) (length * System.Runtime.CompilerServices.Unsafe.SizeOf<T>())), length, NativeMemory.Free);
+    public static unsafe Array<T> AllocAligned(int length, int alignment = 16) => new((nint) NativeMemory.AlignedAlloc((nuint) (length * System.Runtime.CompilerServices.Unsafe.SizeOf<T>()), (nuint) alignment), length, NativeMemory.AlignedFree);
     // todo: public static NativeArray<T> AllocPool(int length, TLSFPool? pool = null) => new((pool ?? TLSFPool.Shared).Alloc((nuint)(length * System.Runtime.CompilerServices.Unsafe.SizeOf<T>()), length, TLSFPool.Shared.Free); -- surely one day i'll make a tlsf arena in C#
 
     public unsafe void Free() {
-        if (Pointer != 0) {
-            FreeInner((void*)Pointer);
-            Pointer = 0;
-            Length = 0;
+        if (Ptr != 0) {
+            FreeArrayInner((void*) Ptr);
+            Ptr = 0;
         }
     }
 
-    public override bool Equals(object? obj) {
-        if (obj is Array<T> other) {
-            return Equals(other);
-        }
-
-        return false;
-    }
-
-    public override int GetHashCode() => (int)Pointer;
-
-    public static bool operator ==(Array<T> left, Array<T> right) => left.Equals(right);
-
-    public static bool operator !=(Array<T> left, Array<T> right) => !(left == right);
+    public override bool Equals(object? obj) => base.Equals(obj) && obj is Array<T> other && Equals(other);
+    public override int GetHashCode() => base.GetHashCode();
+    public static bool operator ==(Array<T>? left, Array<T>? right) => Equals(left, right);
+    public static bool operator !=(Array<T>? left, Array<T>? right) => !Equals(left, right);
 }
