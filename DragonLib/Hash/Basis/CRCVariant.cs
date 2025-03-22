caa323fe8e9178346025acaa0c9c6c90cfc54778
Ada <yretenai@gmail.com>
2025-03-22 18:40:19 +0000

    chore: make CRCVariant a struct

--- DragonLib/Hash/Basis/CRCVariant.cs
+++ DragonLib/Hash/Basis/CRCVariant.cs
@@ -1,3 +1,3 @@
 namespace DragonLib.Hash.Basis;
 
-public record CRCVariant<T>(T Polynomial, T Init, T Xor, bool ReflectIn, bool ReflectOut);
+public record struct CRCVariant<T>(T Polynomial, T Init, T Xor, bool ReflectIn, bool ReflectOut);
