6cd60af85f405ef2e5bdcb7d69baf2d4548faa52
Ada <yretenai@gmail.com>
2025-03-23 16:05:54 +0000

    chore: make IWritableMemoryBuffer inherit IMemoryOwner

--- DragonLib/IO/IWritableMemoryBuffer.cs
+++ DragonLib/IO/IWritableMemoryBuffer.cs
@@ -1,6 +1,8 @@
+using System.Buffers;
+
 namespace DragonLib.IO;
 
-public interface IWritableMemoryBuffer : IMemoryBuffer {
+public interface IWritableMemoryBuffer : IMemoryBuffer, IMemoryOwner<byte> {
 	public new Span<byte> Span { get; }
 	public new Memory<byte> Memory { get; }
 }
