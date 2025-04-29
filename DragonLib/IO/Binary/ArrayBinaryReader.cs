e94a50044128f9f710a09bc6ae8e626ac7b2e156
Ada <yretenai@gmail.com>
2025-04-29 06:08:08 +0100

    chore: split ArrayPoolBinaryReader into ArrayBinaryReader

--- DragonLib/IO/Binary/ArrayBinaryReader.cs
+++ DragonLib/IO/Binary/ArrayBinaryReader.cs
@@ -2,27 +2,15 @@
 //
 // SPDX-License-Identifier: EUPL-1.2
 
-using System.Buffers;
-
 namespace DragonLib.IO.Binary;
 
 public class ArrayBinaryReader : BufferBinaryReader {
-	public ArrayBinaryReader(byte[] array, bool leaveOpen = false) {
-		Array = array;
-		LeaveOpen = leaveOpen;
-	}
+	public ArrayBinaryReader(byte[] array) => Array = array;
 
 	public byte[] Array { get; }
-	public bool LeaveOpen { get; }
 	public override int Position { get; set; }
 
 	public override void ReadBytes(Span<byte> span) => Array.AsSpan(Position, span.Length).CopyTo(span);
 
-	protected override void Dispose(bool disposing) {
-		if (LeaveOpen) {
-			return;
-		}
-
-		ArrayPool<byte>.Shared.Return(Array);
-	}
+	protected override void Dispose(bool disposing) { }
 }
---
+++ DragonLib/IO/Binary/ArrayPoolBinaryReader.cs
@@ -0,0 +1,23 @@
+// SPDX-FileCopyrightText: 2025 Legiayayana
+//
+// SPDX-License-Identifier: EUPL-1.2
+
+using System.Buffers;
+
+namespace DragonLib.IO.Binary;
+
+public class ArrayPoolBinaryReader : ArrayBinaryReader {
+	public ArrayPoolBinaryReader(byte[] array, bool leaveOpen = false) : base(array) {
+		LeaveOpen = leaveOpen;
+	}
+
+	public bool LeaveOpen { get; }
+
+	protected override void Dispose(bool disposing) {
+		if (LeaveOpen) {
+			return;
+		}
+
+		ArrayPool<byte>.Shared.Return(Array);
+	}
+}
